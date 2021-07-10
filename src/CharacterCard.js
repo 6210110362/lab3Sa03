f447fcaad35676bb8496887782fa62ab97b7bd6a
6210110362 <6210110362@psu.ac.th>
2021-07-10 21:33:59 +0700

    part 5

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 
 export default function CharacterCard(props) {
     const [active, setActive] = useState(false);
-    
+
     const activate = () => {
-        setActive(true)
-        props.activationHandler(props.value)
+        if(!active){
+            setActive(true)
+            props.activationHandler(props.value)
+        }
     }
 
     const className = `card ${active ? 'activeCard': ''}`
